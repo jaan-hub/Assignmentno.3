7d70ea1e46d4e81bda098c09da0ce8cc13218200
faijan pathan <official.jaan07@gmail.com>
2025-07-24 20:11:43 +0530

    switch cases

---
+++ q5.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    int days;
+    printf("enter days num(1 to 7):");
+    scanf("%d",&days);
+    switch(days)
+    {
+        case 1:
+        printf("sunday");
+        break;
+        case 2:
+        printf("monday");
+        break;
+        case 3:
+        printf("tuesday");
+        break;
+        case 4:
+        printf("wednesday");
+        break;
+        case 5:
+        printf("thursday");
+        break;
+        case 6:
+        printf("friday");
+        break;
+        case 7:
+        printf("saturday");
+        break;
+        default:
+        printf("invalid day");
+    }
+    return 0;
+}
